843583dc1568e3b8fce01484701a93bcb86dd91e
Khaled Hamed <khaled.hamedt@gmail.com>
2017-03-28 22:15:37 +0200

    P4: Remove swaps
    All swap functions were removed and an external variable used instead.
    Both algorithms are now 2x faster.
    The GIF shows the difference in time before and after the change.
    http://i.imgur.com/4cyP6X3.gif

--- P4/Source.cpp
+++ P4/Source.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <time.h>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 // Returns a vector of size n filled with rands
@@ -15,9 +16,13 @@ inline vector<int> randVector(int n) {
 int normalInsertionSort(vector<int> arr) {
 	clock_t begin = clock();
 
-	for (int i = 1; i < arr.size(); i++)
-		for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--)
-			swap(arr[j], arr[j - 1]);
+	int cur, j;
+	for (int i = 1; i < arr.size(); i++) {
+		cur = arr[i];
+		for (j = i; j > 0 && arr[j - 1] > cur; j--)
+			arr[j] = arr[j - 1];
+		arr[j] = cur;
+	}
 
 	clock_t end = clock();
 	int elapsed_ms = end - begin;
@@ -29,21 +34,25 @@ int doubleInsertionSort(vector<int> arr) {
 
 	for (int i = 1; i < arr.size() - 1; i += 2) {
 		// arr[i] and arr[i + 1] are to be inserted
-		if (arr[i] > arr[i + 1])
-			swap(arr[i], arr[i + 1]);
+		int mn = min(arr[i], arr[i + 1]);
+		int mx = max(arr[i], arr[i + 1]);
 
 		int j;
 		// Keep shifting both while the max of them is smaller than the next element
-		for (j = i; j > 0 && arr[j + 1] < arr[j - 1]; j--)
-			swap(arr[j], arr[j - 1]), swap(arr[j], arr[j + 1]);
+		for (j = i; j > 0 && arr[j - 1] > mx; j--)
+			arr[j + 1] = arr[j - 1];
+		arr[j + 1] = mx;
 
 		// Continue shifting the min of them normally
-		for (; j > 0 && arr[j] < arr[j - 1]; j--)
-			swap(arr[j], arr[j - 1]);
+		for (; j > 0 && arr[j - 1] > mn; j--)
+			arr[j] = arr[j - 1];
+		arr[j] = mn;
 	}
 	// The last element may hasn't been inserted
-	for (int j = arr.size() - 1; j > 0 && arr[j] < arr[j - 1]; j--)
-		swap(arr[j], arr[j - 1]);
+	int last = arr.back(), j;
+	for (j = arr.size() - 1; j > 0 && arr[j - 1] > last; j--)
+		arr[j] = arr[j - 1];
+	arr[j] = last;
 
 	clock_t end = clock();
 	int elapsed_ms = end - begin;
@@ -56,7 +65,7 @@ int main() {
 	
 	cout << "Size\t\tNormal(ms)\tDouble(ms)" << endl;
 	file << "Size,Normal(ms),Double(ms)" << endl;
-	for (int arr_size = 5000; arr_size <= 100000; arr_size += 5000) {
+	for (int arr_size = 50000; arr_size <= 1000000; arr_size += 50000) {
 		vector<int> arr = randVector(arr_size);
 		int normal_result = normalInsertionSort(arr);
 		int double_res = doubleInsertionSort(arr);

b124a2fc5de228d070f505de1eb5dab514d37af7
Khaled Hamed <khaled.hamedt@gmail.com>
2017-04-11 23:30:51 +0200

    P3: Finished

---
+++ DataStructure-Assignment-3/A3_20150101_20150204_G5_G5/A3_P3_20150101_20150204/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "LList.h"
+using namespace std;
+
+template <class T>
+class Queue {
+private:
+	LList<T> list;
+public:
+	Queue() {}
+	Queue(const T& val, int initial_size) {
+		while (initial_size--)
+			list.push_back(val);
+	}
+	~Queue() {
+		while (!list.empty())
+			list.pop_back();
+	}
+
+	T& front() const {
+		if (!empty())
+			return *list.begin();
+		else throw "Trying to get top of empty queue";
+	}
+	void push(const T& val) {
+		list.push_back(val);
+	}
+	void pop() {
+		if (!empty())
+			list.pop_front();
+		else throw "Trying to get top of empty queue";
+	}
+	int size() const {
+		return list.size();
+	}
+	bool empty() const {
+		return list.empty();
+	}
+};
+
+int main() {
+	Queue<int> q;
+	for (int i = 0; i < 10; i++)
+		q.push(i);
+	while (!q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+	cout << endl;
+	cin.get();
+}

795ebdf4c00d458a35ed6f7ca5da869ff436c4ba
Khaled Hamed <khaled.hamedt@gmail.com>
2017-05-03 20:55:28 +0200

    Section#3: Finish Delete

--- DataStructure-Assignment-4/A4_20150101_20150204_G5_G5/Section#3/Source.cpp
+++ DataStructure-Assignment-4/A4_20150101_20150204_G5_G5/Section#3/Source.cpp
@@ -83,7 +83,7 @@ public:
 
 			// To balance the tree one time only
 			if (!balanced)
-				balance(cur_node->left);
+				balanceAddition(cur_node->left);
 		}
 
 		else {
@@ -93,12 +93,17 @@ public:
 
 			// To balance the tree one time only
 			if (!balanced)
-				balance(cur_node->right);
+				balanceAddition(cur_node->right);
 		}
 	}
 
-	void balance(BSTNode<T>* cur_child) {
+	// Balance the tree after a node is added
+	void balanceAddition(BSTNode<T>* cur_child) {
 		balanced = true;
+
+		// We loop from cur_child up to the root
+		// Fixing nodes with wrong balance factor 
+		// by applying the appropriate rotation
 		for (BSTNode<T>* cur_parent = cur_child->parent;
 			cur_parent != NULL;
 			cur_parent = cur_child->parent) {
@@ -106,11 +111,19 @@ public:
 			// and the child that's gonna be under it
 			BSTNode<T> *new_child, *original_parent;
 
+			/*
+				The balance factor hasn't been updated yet, so the deletion is taken into
+				consideration in the if conditions below (it will increase or decrease by one)
+			*/
+
 			// If it's the right child
 			if (cur_child == cur_parent->right) {
 				if (cur_parent->balanceFactor > 0) {
+					// Preserving the current parent as the original one
+					// to connect it to the new child after rotation
 					original_parent = cur_parent->parent;
 
+					// Deciding whether to do single or double rotation
 					if (cur_child->balanceFactor < 0)
 						new_child = rotateRightLeft(cur_parent);
 					else
@@ -121,8 +134,9 @@ public:
 					cur_parent->balanceFactor = 0;
 					break;
 				}
-				else {
+				else { // balanceFactor == 0
 					cur_parent->balanceFactor = 1;
+					// To proceed in the loop
 					cur_child = cur_parent;
 					continue;
 				}
@@ -131,8 +145,11 @@ public:
 			// If it's the left child
 			else {
 				if (cur_parent->balanceFactor < 0) {
+					// Preserving the current parent as the original one
+					// to connect it to the new child after rotation
 					original_parent = cur_parent->parent;
 
+					// Deciding whether to do single or double rotation
 					if (cur_child->balanceFactor > 0)
 						new_child = rotateLeftRight(cur_parent);
 					else
@@ -143,16 +160,21 @@ public:
 					cur_parent->balanceFactor = 0;
 					break;
 				}
-				else {
+				else { // balanceFactor == 0
 					cur_parent->balanceFactor = -1;
+					// To proceed in the loop
 					cur_child = cur_parent;
 					continue;
 				}
 			}
 
+			// This part will only be reached if a rotation is done
+			// Otherwise it will just break or continue from the part above
+
 			// Connect the original parent and the new child
 			new_child->parent = original_parent;
 			if (original_parent) {
+				// If the old child was the original parent's left
 				if (&cur_parent == &original_parent->left)
 					original_parent->left = new_child;
 				else
@@ -164,7 +186,7 @@ public:
 		}
 	}
 
-	// Returns the new root after rotation
+	// Returns the new root after rotating the subtree
 	BSTNode<T>* rotateLeft(BSTNode<T>*& cur_node) {
 		BSTNode<T>* right_node = cur_node->right;
 
@@ -189,7 +211,7 @@ public:
 		return right_node;
 	}
 
-	// Returns the new root after rotation
+	// Returns the new root after rotating the subtree
 	BSTNode<T>* rotateRight(BSTNode<T>*& cur_node) {
 		BSTNode<T>* left_node = cur_node->left;
 
@@ -214,23 +236,156 @@ public:
 		return left_node;
 	}
 
+	// Returns the new root after doubly rotating the subtree
 	BSTNode<T>* rotateRightLeft(BSTNode<T>*& cur_node) {
 		cur_node->right = rotateRight(cur_node->right);
 
 		return rotateLeft(cur_node);
 	}
 
+	// Returns the new root after doubly rotating the subtree
 	BSTNode<T>* rotateLeftRight(BSTNode<T>*& cur_node) {
 		cur_node->left = rotateLeft(cur_node->left);
 
 		return rotateRight(cur_node);
 	}
+
+	void deleteValue(const T& key) {
+		BSTNode<T>* cur_node = search(key);
+		if (cur_node)
+			deleteNode(cur_node);
+	}
+
+	void deleteNode(BSTNode<T>*& cur_node) {
+		// If it has both children or right child only
+		// Get
+		if (cur_node->right && cur_node->left || cur_node->right) {
+			BSTNode<T>* successor = cur_node->right;
+			while (successor->left)
+				successor = successor->left;
+			cur_node->key = successor->key;
+			deleteNode(successor);
+		}
+
+		else if (cur_node->left) {
+			BSTNode<T>* successor = cur_node->left;
+			while (successor->right)
+				successor = successor->right;
+			cur_node->key = successor->key;
+			deleteNode(successor);
+		}
+
+		else {
+			balanceDeletion(cur_node);
+			if (cur_node->parent->right == cur_node)
+				cur_node->parent->right = NULL;
+			else if (cur_node->parent->left == cur_node)
+				cur_node->parent->left = NULL;
+			delete cur_node;
+		}
+	}
+
+	// Balance the tree after a node is deleted
+	void balanceDeletion(BSTNode<T>* cur_child) {
+
+		// We loop from cur_child up to the root
+		// Fixing nodes with wrong balance factor 
+		// by applying the appropriate rotation
+		for (BSTNode<T>* cur_parent = cur_child->parent;
+			cur_parent != NULL;
+			cur_parent = cur_child->parent) {
+
+			// For the original parent before any rotating
+			// and the child that's gonna be under it
+			BSTNode<T> *new_child, *original_parent, *other_side_child;
+			/*
+				other_side_child is because, unlike adding, we are on the lighter side of
+				the tree, so to see if we're gonna do a signle or double rotation we
+				need to know the balance factor of the child of the other side.
+
+				The balance factor hasn't been updated yet, so the deletion is taken into
+				consideration in the if conditions below (it will increase or decrease by one)
+			*/
+
+			// If it's the right child
+			if (cur_child == cur_parent->right) {
+				if (cur_parent->balanceFactor < 0) {
+					// Preserving the current parent as the original one
+					// to connect it to the new child after rotation
+					original_parent = cur_parent->parent;
+
+					// Deciding whether to do single or double rotation
+					other_side_child = cur_parent->left;
+					if (other_side_child->balanceFactor > 0)
+						new_child = rotateLeftRight(cur_parent);
+					else
+						new_child = rotateRight(cur_parent);
+				}
+				else if (cur_parent->balanceFactor == 0) {
+					cur_parent->balanceFactor = -1;
+					break;
+				}
+				else { // balanceFactor < 0
+					cur_parent->balanceFactor = 0;
+					// To proceed in the loop
+					cur_child = cur_parent;
+					continue;
+				}
+			}
+
+			// If it's the left child
+			else {
+				if (cur_parent->balanceFactor > 0) {
+					// Preserving the current parent as the original one
+					// to connect it to the new child after rotation
+					original_parent = cur_parent->parent;
+
+					// Deciding whether to do single or double rotation
+					other_side_child = cur_parent->right;
+					if (other_side_child->balanceFactor < 0)
+						new_child = rotateRightLeft(cur_parent);
+					else
+						new_child = rotateLeft(cur_parent);
+				}
+				else if (cur_parent->balanceFactor == 0) {
+					cur_parent->balanceFactor = 1;
+					break;
+				}
+				else { // balanceFactor < 0
+					cur_parent->balanceFactor = 0;
+					// To proceed in the loop
+					cur_child = cur_parent;
+					continue;
+				}
+			}
+
+			// This part will only be reached if a rotation is done
+			// Otherwise it will just break or continue from the part above
+
+			// Connect the original parent and the new child
+			new_child->parent = original_parent;
+			if (original_parent) {
+				// If the old child was the original parent's left
+				if (&cur_parent == &original_parent->left)
+					original_parent->left = new_child;
+				else
+					original_parent->right = new_child;
+			}
+			else
+				root = new_child;
+			break;
+		}
+	}
 };
 
 int main() {
-	BSTAVL<int> x;
-	for (int i = 0; i < 1000; i++)
+	BSTAVL<double> x;
+	for (int i = 0; i < 7; i++)
 		x.insert(i);
+	x.insert(2.5);
+	x.printInOrder();
+	x.deleteValue(6);
+	x.deleteValue(4);
 	x.printInOrder();
 	cin.get();
 }

f7f010210009471b06de52a0905e48e10da34810
Khaled Hamed <khaled.hamedt@gmail.com>
2017-10-04 22:49:36 +0200

    Implement cd

--- OS/CLI/src/com/example/CLI.java
+++ OS/CLI/src/com/example/CLI.java
@@ -1,16 +1,68 @@
 package com.example;
 
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_ja;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class CLI {
-	static String currentDirectory = System.getProperty("user.dir");
+	static String currentDir = System.getProperty("user.dir");
+
+	public static boolean isPath(String path) {
+		File file = new File(path);
+
+		if (!file.isAbsolute())
+			file = new File(currentDir + "/" + path);
+
+		if (!file.exists()) {
+			System.out.println("Directory doesn't exist");
+			return false;
+		}
+		else if (!file.isDirectory()) {
+			System.out.println("This is not a directory");
+			return false;
+		}
+		return true;
+	}
+
+	public static String removeDots(String path) {
+		path = path.replaceAll("([.])", "");
+		return path;
+	}
+
+	public static void cd(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Too many arguments!");
+			return;
+		}
+		if (!isPath(args[1])) return;
+
+		Path path = Paths.get(args[1]);
+		if (!path.isAbsolute())
+			path = Paths.get(currentDir + "/" + args[1]);
+
+		path = path.normalize();
+		path = Paths.get(removeDots(path.toString()));
+		currentDir = path.toString();
+		System.setProperty("user.dir", currentDir);
+	}
 
 	public static void main(String[] args) {
 		while (true) {
-			System.out.print(currentDirectory + ": ");
+			System.out.print(currentDir + ": ");
 			Scanner scan = new Scanner(System.in);
 			String cmd = scan.nextLine();
-			System.out.println(cmd);
+
+			String[] arguments = cmd.split(" ");
+
+			switch (arguments[0]) {
+				case "cd":
+					cd(arguments);
+					break;
+			}
 		}
 	}
 }

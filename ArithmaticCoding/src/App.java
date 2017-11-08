b7acb7f9b698b2ef2cfc4fde2ba0373908fe0a9e
Khaled Hamed <khaled.hamedt@gmail.com>
2017-11-08 15:32:30 +0200

    Write/read files

--- ArithmaticCoding/src/App.java
+++ ArithmaticCoding/src/App.java
@@ -3,13 +3,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
+import java.nio.file.*;
+import java.util.*;
 
 public class App {
 	private JPanel panelMain;
@@ -17,7 +12,7 @@ public class App {
 	private JButton compressButton;
 	private JButton decompressButton;
 
-//	private ArithmaticCoding compressor = new ArithmaticCoding();
+	private ArithmaticCoding compressor;
 	private JFileChooser fileChooser = new JFileChooser(new File(System.getProperty("user.dir")));
 	private String parentDir;
 
@@ -25,50 +20,54 @@ public class App {
 		FileNameExtensionFilter filter = new FileNameExtensionFilter(".txt", "txt", "text");
 		fileChooser.setFileFilter(filter);
 
+		ArrayList<Character> symbols = new ArrayList<>();
+		ArrayList<Double> probs = new ArrayList<>();
+		symbols.add('A');
+		symbols.add('B');
+		symbols.add('C');
+		probs.add(0.8);
+		probs.add(0.02);
+		probs.add(0.18);
+
+		compressor = new ArithmaticCoding(symbols, probs);
+
 		chooseFileButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				fileChooser.showOpenDialog(null);
 			}
 		});
-//		compressButton.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				File file = fileChooser.getSelectedFile();
-//				String[] lines = readFile(file.toPath().toString());
-//
-//				Huffman.HashResult hash = compressor.encode(String.join("\n", lines));
-//
-//				StringJoiner hashStr = new StringJoiner(",");
-//				for (Huffman.Pair row : hash.codeTable) {
-//					hashStr.add(row.letter).add(row.code);
-//				}
-//				hashStr.add(hash.code);
-//
-//				parentDir = file.toPath().getParent().toString();
-//				writeFile(parentDir + "/compressed.txt", hashStr.toString());
-//			}
-//		});
-//		decompressButton.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				String[] lines = readFile(parentDir + "/compressed.txt");
-//
-//				String[] hashStr = String.join("\n", lines).split(",");
-//				Huffman.HashResult hash = new Huffman.HashResult();
-//				hash.code = hashStr[hashStr.length - 1];
-//				hash.codeTable = new Huffman.Pair[(hashStr.length - 1) / 2];
-//				for (int i = 0; i < hashStr.length - 1; i += 2) {
-//					hash.codeTable[i / 2] = new Huffman.Pair(hashStr[i], hashStr[i + 1]);
-//				}
-//
-//				writeFile(parentDir + "/decompressed.txt", compressor.decode(hash));
-//			}
-//		});
+
+		compressButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				File file = fileChooser.getSelectedFile();
+				parentDir = file.toPath().getParent().toString();
+				String[] lines = readFile(file.toPath().toString());
+
+				String hash = compressor.encode(String.join("\n", lines));
+				hash = compressor.txtSize + "\n" + hash;
+
+				writeFile(parentDir + "/compressed.txt", hash);
+			}
+		});
+
+		decompressButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String[] lines = readFile(parentDir + "/compressed.txt");
+				compressor.txtSize = Integer.parseInt(lines[0]);
+				lines = Arrays.copyOfRange(lines, 1, lines.length);
+
+				String decoded = compressor.decode(String.join("\n", lines));
+
+				writeFile(parentDir + "/decompressed.txt", decoded);
+			}
+		});
 	}
 
 	private static void writeFile(String pathStr, String text) {
-		List<String> linesList = Arrays.asList(text);
+		List<String> linesList = Collections.singletonList(text);
 		Path path = Paths.get(pathStr);
 		try {
 			Files.write(path, linesList);
@@ -89,12 +88,12 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-//		JFrame frame = new JFrame("App");
-//		frame.setContentPane(new App().panelMain);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.pack();
-//		frame.setResizable(false);
-//		frame.setVisible(true);
+		JFrame frame = new JFrame("App");
+		frame.setContentPane(new App().panelMain);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
 
 		ArrayList<Character> symbols = new ArrayList<>();
 		ArrayList<Double> probs = new ArrayList<>();

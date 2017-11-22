9aca2f604253373a8396e41564c913c6ec4b43cf
Mohamed Refaie <mohamed1refaie@hotmail.com>
2017-11-22 19:38:47 +0200

    Scheduling SJF

---
+++ src/process.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+
+public class process {
+    String name;
+    int arrival;
+    int burst;
+    int remaining;
+    int lastEntered;
+    int waitingTime;
+    int TurnAround;
+
+    public process(String name, int arrival, int burst, int remaining, int lastEntered, int waitingTime, int turnAround) {
+        this.name = name;
+        this.arrival = arrival;
+        this.burst = burst;
+        this.remaining = remaining;
+        this.lastEntered = lastEntered;
+        this.waitingTime = waitingTime;
+        TurnAround = turnAround;
+    }
+
+
+
+
+}

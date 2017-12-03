dba6e1383a4d13681e14cff67996a4039ae32b60
Khaled Hamed <khaled.hamedt@gmail.com>
2017-12-03 22:40:29 +0200

    Main

--- TodoApp/src/Main.java
+++ TodoApp/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-	public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+	static TodoApplication app = new TodoApplication();
+
+	public static void main(String[] args) {
+		System.out.println("login or signup?");
+		Scanner sc = new Scanner(System.in);
+		String res;
+		res = sc.nextLine();
+
+		if (res.equals("signup")) {
+			String name, mail, password;
+			System.out.print("Name: "); name = sc.nextLine();
+			System.out.print("Mail: "); mail = sc.nextLine();
+			System.out.print("Password: "); password = sc.nextLine();
+			app.addUser(name, mail, password);
+			app.login(name, password);
+		}
+		else {
+			String name, password;
+			System.out.print("Name: "); name = sc.nextLine();
+			System.out.print("Password: "); password = sc.nextLine();
+			app.login(name, password);
+		}
+
+		while (true) {
+			// TODO:
+		}
 	}
 }
--- TodoApp/src/TodoApplication.java
+++ TodoApp/src/TodoApplication.java
@@ -1,5 +1,14 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TodoApplication {
-	ArrayList<User> users = new ArrayList<>();
+	HashMap<String, User> users = new HashMap<>();
+	String currentUser = "";
+
+	public void addUser(String name, String mail, String password) {
+		users.put(name, new User(name, mail, password));
+	}
+
+	public boolean login(String name, String password) {
+		return users.get(name).account.password.equals(password);
+	}
 }

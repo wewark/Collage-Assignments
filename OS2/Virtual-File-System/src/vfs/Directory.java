8da23d94afd7ae1ac42eb9c93c9b9ed12488fa4b
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-31 00:13:06 +0200

    Refactoring

--- OS2/Virtual-File-System/src/vfs/Directory.java
+++ OS2/Virtual-File-System/src/vfs/Directory.java
@@ -33,4 +33,97 @@ class Directory implements Serializable {
 			System.out.println("*" + filename);
 		}
 	}
+
+	public static void createFile(String pathStr, int size) {
+		String[] path = pathStr.split("/");
+		if (!valid(path)) {
+			System.out.println("invalid path");
+			return;
+		}
+
+		Directory cur = getParent(path);
+		String filename = path[path.length - 1];
+		if (cur.files.containsKey(filename)) {
+			System.out.println("file already exists");
+			return;
+		}
+
+		System.out.println("File created");
+		cur.files.put(filename,
+				new File(pathStr, filename, size, AllocationMethod.BESTFIT)); // temp
+	}
+
+	public static void createFolder(String pathStr) {
+		String[] path = pathStr.split("/");
+		if (!valid(path)) {
+			System.out.println("invalid path");
+			return;
+		}
+
+		Directory cur = getParent(path);
+		String dirname = path[path.length - 1];
+		if (cur.directories.containsKey(dirname)) {
+			System.out.println("directory already exists");
+			return;
+		}
+
+		System.out.println("Folder created");
+		cur.directories.put(dirname, new Directory(pathStr, dirname));
+	}
+
+	public static void deleteFile(String pathStr) {
+		String[] path = pathStr.split("/");
+		if (!valid(path)) {
+			System.out.println("invalid path");
+			return;
+		}
+
+		Directory cur = getParent(path);
+		String filename = path[path.length - 1];
+		if (!cur.files.containsKey(filename)) {
+			System.out.println("file doesn't exist");
+			return;
+		}
+
+		System.out.println("File deleted");
+		cur.files.remove(filename);
+	}
+
+	public static void deleteFolder(String pathStr) {
+		String[] path = pathStr.split("/");
+		if (!valid(path)) {
+			System.out.println("invalid path");
+			return;
+		}
+
+		Directory cur = getParent(path);
+		String dirname = path[path.length - 1];
+		if (!cur.directories.containsKey(dirname)) {
+			System.out.println("directory doesn't exist");
+			return;
+		}
+
+		System.out.println("Folder deleted");
+		cur.directories.remove(dirname);
+	}
+
+	private static boolean valid(String[] path) {
+		if (!path[0].equals("root")) return false;
+		Directory cur = root;
+		for (int i = 1; i < path.length - 1; ++i) {
+			if (!cur.directories.containsKey(path[i]))
+				return false;
+			cur = cur.directories.get(path[i]);
+		}
+
+		return true;
+	}
+
+	private static Directory getParent(String[] path) {
+		Directory ret = root;
+		for (int i = 1; i < path.length - 1; ++i) {
+			ret = ret.directories.get(path[i]);
+		}
+		return ret;
+	}
 }
--- OS2/Virtual-File-System/src/vfs/Main.java
+++ OS2/Virtual-File-System/src/vfs/Main.java
@@ -19,16 +19,16 @@ public class Main {
 					save();
 					return;
 				case "CreateFile":
-					createFile(args[1], Integer.parseInt(args[2]));
+					Directory.createFile(args[1], Integer.parseInt(args[2]));
 					break;
 				case "CreateFolder":
-					createFolder(args[1]);
+					Directory.createFolder(args[1]);
 					break;
 				case "DeleteFile":
-					deleteFile(args[1]);
+					Directory.deleteFile(args[1]);
 					break;
 				case "DeleteFolder":
-					deleteFolder(args[1]);
+					Directory.deleteFolder(args[1]);
 					break;
 				case "DisplayDiskStructure":
 					Directory.root.print(0);
@@ -38,99 +38,6 @@ public class Main {
 					break;
 			}
 		}
-	}
-
-	private static void createFile(String pathStr, int size) {
-		String[] path = pathStr.split("/");
-		if (!valid(path)) {
-			System.out.println("invalid path");
-			return;
-		}
-
-		Directory cur = getParent(path);
-		String filename = path[path.length - 1];
-		if (cur.files.containsKey(filename)) {
-			System.out.println("file already exists");
-			return;
-		}
-
-		System.out.println("File created");
-		cur.files.put(filename,
-				new File(pathStr, filename, size, AllocationMethod.BESTFIT)); // temp
-	}
-
-	private static void createFolder(String pathStr) {
-		String[] path = pathStr.split("/");
-		if (!valid(path)) {
-			System.out.println("invalid path");
-			return;
-		}
-
-		Directory cur = getParent(path);
-		String dirname = path[path.length - 1];
-		if (cur.directories.containsKey(dirname)) {
-			System.out.println("directory already exists");
-			return;
-		}
-
-		System.out.println("Folder created");
-		cur.directories.put(dirname, new Directory(pathStr, dirname));
-	}
-
-	private static void deleteFile(String pathStr) {
-		String[] path = pathStr.split("/");
-		if (!valid(path)) {
-			System.out.println("invalid path");
-			return;
-		}
-
-		Directory cur = getParent(path);
-		String filename = path[path.length - 1];
-		if (!cur.files.containsKey(filename)) {
-			System.out.println("file doesn't exist");
-			return;
-		}
-
-		System.out.println("File deleted");
-		cur.files.remove(filename);
-	}
-
-	private static void deleteFolder(String pathStr) {
-		String[] path = pathStr.split("/");
-		if (!valid(path)) {
-			System.out.println("invalid path");
-			return;
-		}
-
-		Directory cur = getParent(path);
-		String dirname = path[path.length - 1];
-		if (!cur.directories.containsKey(dirname)) {
-			System.out.println("directory doesn't exist");
-			return;
-		}
-
-		System.out.println("Folder deleted");
-		cur.directories.remove(dirname);
-	}
-
-	private static boolean valid(String[] path) {
-		if (!path[0].equals("root")) return false;
-		Directory cur = Directory.root;
-		for (int i = 1; i < path.length - 1; ++i) {
-			if (!cur.directories.containsKey(path[i]))
-				return false;
-			cur = cur.directories.get(path[i]);
-		}
-
-		return true;
-	}
-
-	private static Directory getParent(String[] path) {
-		Directory ret = Directory.root;
-		for (int i = 1; i < path.length - 1; ++i) {
-			ret = ret.directories.get(path[i]);
-		}
-		return ret;
 	}
 
 	private static void save() {
@@ -156,11 +63,9 @@ public class Main {
 			fileIn.close();
 		} catch (IOException i) {
 			i.printStackTrace();
-			return;
 		} catch (ClassNotFoundException c) {
 			System.out.println("class not found");
 			c.printStackTrace();
-			return;
 		}
 	}
 }

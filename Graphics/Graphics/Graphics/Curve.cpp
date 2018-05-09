9e6fef6c7a6835e6e8cb861291b609a40e00dc9a
Khaled Hamed <khaled.hamedt@gmail.com>
2018-05-10 00:11:07 +0200

    Fix hermite curve

--- Graphics/Graphics/Graphics/Curve.cpp
+++ Graphics/Graphics/Graphics/Curve.cpp
@@ -1,19 +1,22 @@
 #include "stdafx.h"
 #include "Curve.h"
 
-void Hermite(HDC hdc, int x1, int y1, int x2, int y2,
+void Hermite(HDC hdc, int px1, int py1, int x2, int y2,
 	int x3, int y3, int x4, int y4) {
+	x2 -= px1, x4 -= x3;
+	y2 -= py1, y4 -= y3;
+
 	int a1, b1, c1, d1;
-	a1 = 2 * x1 + x2 - 2 * x3 + x4;
-	b1 = -3 * x1 - 2 * x2 + 3 * x3 - x4;
+	a1 = 2 * px1 + x2 - 2 * x3 + x4;
+	b1 = -3 * px1 - 2 * x2 + 3 * x3 - x4;
 	c1 = x2;
-	d1 = x1;
+	d1 = px1;
 
 	int a2, b2, c2, d2;
-	a2 = 2 * y1 + y2 - 2 * y3 + y4;
-	b2 = -3 * y1 - 2 * y2 + 3 * y3 - y4;
+	a2 = 2 * py1 + y2 - 2 * y3 + y4;
+	b2 = -3 * py1 - 2 * y2 + 3 * y3 - y4;
 	c2 = y2;
-	d2 = y1;
+	d2 = py1;
 
 	double x, y;
 	for (double t = 0; t <= 1; t += 0.0001) {

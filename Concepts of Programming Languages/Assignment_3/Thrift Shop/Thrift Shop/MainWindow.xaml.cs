d4371d9edf63258f7e3367fb2461e5af561edd02
Khaled Hamed <khaled.hamedt@gmail.com>
2018-12-17 19:02:25 +0200

    init project

---
+++ Concepts of Programming Languages/Assignment_3/Thrift Shop/Thrift Shop/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace Thrift_Shop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SQLiteConnection.CreateFile("my_db.sqlite");
+            SQLiteConnection con = new SQLiteConnection("Data Source=my_db.sqlite;Version=3");
+            con.Open();
+
+            string sql = "CREATE TABLE highscores (name VARCHAR(20), score INT)";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+
+
+            label1.Content = "hi";
+        }
+    }
+}
